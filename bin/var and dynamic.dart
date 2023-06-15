212e613d25c2e0b7162ddd98632905324a356241
hisham843 <hishammohammed843@gmail.com>
2023-06-16 02:22:53 +0530

    test programs

---
+++ bin/var and dynamic.dart
@@ -0,0 +1,20 @@
+void main(){
+
+  //int data ="hello";
+
+  var data=1000.8,data1="hello";
+  //var data1 ="hello";
+
+  print("data =$data");
+  print("data1=$data1");
+
+  //Dynamic --->
+
+  dynamic data2="hello";
+          data2=1006.8;
+          data2=2000;
+          data2="machu";
+
+          print("data2=$data2");
+
+}
